05dab88c66c983de5afea326ba8e48aa0e7a6aa4
drabina <krzysiek.drobinski@gmail.com>
2022-12-07 23:20:54 +0100

    Enable rust-mode on startup if required

--- init.el
+++ init.el
@@ -20,3 +20,9 @@
 (load "~/.emacs.d/drobinsk/drabina-diff-mode.el")
 (eval-after-load "diff-mode"
   '(update-diff-refine-colors))
+
+(add-to-list 'load-path "~/.emacs.d//rust-mode/")
+(autoload 'rust-mode "rust-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
