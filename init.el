d9a640cb683dadf035656a306170c468703d2b19
drabina <krzysiek.drobinski@gmail.com>
2022-12-07 23:09:59 +0100

    Enable spacemacs-dark theme on startup

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -10,6 +12,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(add-to-list 'load-path "~/.emacs.d/spacemacs-theme")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/spacemacs-theme")
+(package-initialize)
+(load-theme 'spacemacs-dark t)
+
 (load "~/.emacs.d/drobinsk/drabina-diff-mode.el")
 (eval-after-load "diff-mode"
   '(update-diff-refine-colors))
